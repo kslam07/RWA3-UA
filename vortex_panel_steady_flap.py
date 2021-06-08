029f542aa9fbc464a7039fc4477e138894a870fe
Arthur Blomme <47719562+bloempje@users.noreply.github.com>
2021-06-08 19:33:05 +0200

    Cherry-pick steady flap - remove JIT implementation

--- vortex_panel_steady_flap.py
+++ vortex_panel_steady_flap.py
@@ -1,44 +1,41 @@
+
 ## VORTEX PANEL METHOD
 
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.cm import ScalarMappable
-import numba as nb  # trying out write unvectorized code with Numba jitting
-import warnings
-
-warnings.simplefilter('ignore', category=nb.errors.NumbaPerformanceWarning)
 
 # ---------------------------------- #
 # Flags
 # ---------------------------------- #
 
-enable_flap = True
+enable_flap         = True
 
-plot_backmesh = False
-plot_camber = False
-plot_cl_curve = True
+plot_backmesh       = False
+plot_camber         = False
+plot_cl_curve       = True
 plot_velocity_field = True
 plot_pressure_field = True
-plot_deltaP = False
+plot_deltaP         = False
 
 # ---------------------------------- #
 # Geometry and operations parameters
 # ---------------------------------- #
 
 # Airfoil
-Npan = 100  # Number of panels
-res = 50  # Grid discretization in 1 direction
-c = 1  # (m) chord length
+Npan = 100             # Number of panels
+res = 50               # Grid discretization in 1 direction
+c = 1                  # (m) chord length
 
 # Flap
-c_flap = 0.3  # Fraction of chord length
-a_flap = 15  # (deg) Flap deflection in degrees
-Npan_flap = 10  # Number of panels on flap
+c_flap = 0.3           # Fraction of chord length
+a_flap = 15            # (deg) Flap deflection in degrees
+Npan_flap = 10         # Number of panels on flap
 
 # Operations
-rho = 1.225  # (kg/m^3) free-stream density
-U_0 = 10  # (m/s) free-stream velocity
-arange = np.arange(-4, 15)  # Range of AoA
+rho = 1.225                     # (kg/m^3) free-stream density
+U_0 = 10                        # (m/s) free-stream velocity
+arange = np.arange(-4, 15)      # Range of AoA
 cl_arr = np.zeros(len(arange))  # Lift coefficient log
 
 # ---------------------------------- #
@@ -47,10 +44,9 @@ cl_arr = np.zeros(len(arange))  # Lift coefficient log
 
 print('...Creating background grid and refinement.')
 # Add refinement in the vicinity of the flat plate
-xrange = np.linspace(-1.0 * c, 2.0 * c, res) * (1 - 0.2 * np.sin(np.linspace(0, np.pi, res)))
-yrange = np.linspace(-1.5 * c, 1.5 * c, res) * (1 - 0.9 * np.sin(np.linspace(0, np.pi, res)))
+xrange = np.linspace(-1.0 * c, 2.0 * c, res)*(1 - 0.2 * np.sin(np.linspace(0, np.pi, res)))
+yrange = np.linspace(-1.5 * c, 1.5 * c, res)*(1 - 0.9 * np.sin(np.linspace(0, np.pi, res)))
 [X, Y] = np.meshgrid(xrange, yrange)
-
 
 # ---------------------------------- #
 # Vortex Panel Functions
@@ -61,60 +57,21 @@ def naca4(c, m, p, x):  # Calculate corresponding Y components of camber line fo
     print('...Building NACA 4series camber line.')
     # c = hord length,m = max camber, p = max camber postion, x are x coordinates
 
-    x1 = x[np.where(x <= p)]  # X components upto largest thickness point
-    x2 = x[np.where(x > p)]  # X components after largest thickness point
+    x1 = x[np.where(x <= p)]    # X components upto largest thickness point
+    x2 = x[np.where(x > p)]     # X components after largest thickness point
 
-    y1 = m / p ** 2 * (2 * p * x1 - x1 ** 2)  # y with corresponding x1
-    y2 = m / (1 - p) ** 2 * ((1 - 2 * p) + 2 * p * x2 - x2 ** 2)  # y with corresponding x2
-    y = np.concatenate((y1, y2))  # Combined y
+    y1 = m / p ** 2 * (2 * p * x1 - x1 ** 2)                        # y with corresponding x1
+    y2 = m / (1 - p) ** 2 * ((1 - 2 * p) + 2 * p * x2 - x2 ** 2)    # y with corresponding x2
+    y = np.concatenate((y1, y2))                                    # Combined y
 
     return y
-
-
-@nb.njit
-def lumpvor2d(xcol, zcol, xvor, zvor, circvor=1):
-    """
-    Compute the velocity at an arbitrary collocation point (xcol, zcol) due
-    to vortex element of circulation circvor, placed at (xvor, zvor).
-
-    :param xcol: x-coordinate of the collocation point
-    :param zcol: z-coordinate of the collocation point
-    :param xvor: x-coordinate of the vortex
-    :param zvor: z-coordinate of the vortex
-    :param circvor: circulation strength of the vortex (base units)
-
-    :return: 1D array containing the velocity vector (u, w) (x-comp., z-comp.)
-    :rtype: ndarray
-
-    """
-
-    # transformation matrix for the x, z distance between two points
-    dcm = np.array([[0.0, 1.0],
-                    [-1.0, 0.0]])
-
-    # magnitude of the distance between two points
-    r_vortex_sq = (xcol - xvor) ** 2 + (zcol - zvor) ** 2
-
-    if r_vortex_sq < 1e-8:  # some arbitrary threshold
-        return np.array([0.0, 0.0])
-
-    # the distance in x, and z between two points
-    dist_vec = np.array([xcol - xvor, zcol - zvor])
-
-    norm_factor = circvor / (2.0 * np.pi * r_vortex_sq)  # circulation at
-    # vortex element / circumferential distance
-
-    # induced velocity of vortex element on collocation point
-    vel_vor = norm_factor * dcm @ dist_vec
-
-    return vel_vor
 
 
 def indvel(gammaj, x, y, xj, yj):  # Formula 11.1
 
     rj_sqrt = (x - xj) ** 2 + (y - yj) ** 2
-    randommatrix = np.array([[0, 1], [-1, 0]])
-    xymatrix = np.array([[x - xj], [y - yj]])
+    randommatrix = np.matrix('0 1; -1 0')
+    xymatrix = np.matrix([[x - xj], [y - yj]])
     uv = gammaj / (2 * np.pi * rj_sqrt) * np.matmul(randommatrix, xymatrix)
 
     return uv
@@ -123,41 +80,45 @@ def indvel(gammaj, x, y, xj, yj):  # Formula 11.1
 def aijmatrix(xi, yi, xj, yj, Npan, ni):  # Calculation of the influence coefficients
 
     print('   ...Computing influence matrix.')
-    a_mat = np.zeros((Npan, Npan))
+    aijmatrix = np.matrix(np.zeros((Npan, Npan)))
 
     print('   ...Computing induced velocities.')
     for i in range(0, Npan):
 
         for j in range(0, Npan):
-            vel_vor = lumpvor2d(xi[i], yi[i], xj[j], yj[j])
-            a_ij = vel_vor @ ni[:, i]
-            a_mat[i, j] = a_ij
 
-    return a_mat
+            uvmatrix = np.transpose(indvel(1, xi[i], yi[i], xj[j], yj[j]))  # Calculate (u,w), Formula 11.1
+            nmatrix = np.transpose(np.matrix([ni[0, i], ni[1, i]]))         # Prepare ni matrix
+            aij = np.matmul(uvmatrix, nmatrix)                              # Formula 11.5
+            aijmatrix[i, j] = aij                                           # Define aijmatrix
+
+    return aijmatrix
 
 
 def calculation(y, x, Npan, Npan_flap, alpha, a_flap, c, c_flap, U_0, rho):
+
     print('   ...Creating geometry.')
 
     # Add a flap (OPTIONAL)
     if enable_flap:
+
         x = x * (1 - c_flap)
 
         Nf_points = Npan_flap + 1
         x_flap = np.arange(1, Nf_points + 1)
-        x_flap = c_flap * c / 2 * (1 - np.cos((x_flap - 1) * np.pi / (Nf_points - 1)))
+        x_flap = c_flap * c/2 * (1 - np.cos((x_flap - 1) * np.pi/(Nf_points - 1)))
         y_flap = np.copy(x_flap) * 0.
 
         xp_flap = x_flap * np.cos(a_flap) + y_flap * np.sin(a_flap)
         yp_flap = -x_flap * np.sin(a_flap) + y_flap * np.cos(a_flap)
 
-        x = np.concatenate((x, xp_flap[1:] + (1 - c_flap)))
+        x = np.concatenate((x, xp_flap[1:]+ (1 - c_flap)))
         y = np.concatenate((y, yp_flap[1:]))
 
         Npan = Npan + Npan_flap
 
     # center LE at the origin
-    x = x - c / 4
+    x = x - c/4
 
     # Rotate airfoil clockwise
     xp = x * np.cos(alpha) + y * np.sin(alpha)
@@ -166,59 +127,35 @@ def calculation(y, x, Npan, Npan_flap, alpha, a_flap, c, c_flap, U_0, rho):
     # Calculate dx,dy,dc component per panel (dc = panel length)
     dx = np.delete(np.roll(xp, -1) - xp, -1)
     dy = np.delete(np.roll(yp, -1) - yp, -1)
-    dc = np.sqrt(dx ** 2 + dy ** 2)
+    dc = np.sqrt(dx**2 + dy**2)
 
     # Further induced geometry calculations
-    alpha_i = np.arctan2(dy, dx)  # Induced AoA by panel slope
-    ni = np.array([np.sin(-alpha_i), np.cos(-alpha_i)])  # Normal vector; First index = x, second index = y
+    alpha_i = np.arctan2(dy, dx)                            # Induced AoA by panel slope
+    ni = np.matrix([np.sin(-alpha_i), np.cos(-alpha_i)])    # Normal vector; First index = x, second index = y
 
     # Calculate x,y coordinates of the quarter cord (c4) and collocation points (cp)
-    xc4 = xp[0:-1] + dx / 4
-    yc4 = yp[0:-1] + dy / 4
+    xc4 = xp[0:-1] + dx/4
+    yc4 = yp[0:-1] + dy/4
 
-    xcp = xp[0:-1] + dx * (3 / 4)
-    ycp = yp[0:-1] + dy * (3 / 4)
+    xcp = xp[0:-1] + dx * (3/4)
+    ycp = yp[0:-1] + dy * (3/4)
 
     print('   ...Solving linear system for circulation strengths.')
     # Solve system #
-    aij_mat = aijmatrix(xcp, ycp, xc4, yc4, Npan, ni)  # aij matrix
-    RHS = U_0 * np.sin(alpha_i) * np.ones(Npan)  # RHS vector
-    gammamatrix = np.linalg.solve(aij_mat, RHS)  # Find circulation of each vortex point
+    aij_mat = aijmatrix(xcp, ycp, xc4, yc4, Npan, ni)   # aij matrix
+    RHS = U_0 * np.sin(alpha_i) * np.ones(Npan)         # RHS vector
+    gammamatrix = np.linalg.solve(aij_mat, RHS)         # Find circulation of each vortex point
 
     print('   ...Calculating lift and pressure.\n')
     # Secondary computations
-    dLj = rho * U_0 * gammamatrix  # Lift difference
-    L = np.sum(dLj)  # Total Lift
-    Cl = L / (0.5 * rho * U_0 ** 2 * c)  # Lift coefficient
+    dLj = rho * U_0 * gammamatrix           # Lift difference
+    L = np.sum(dLj)                         # Total Lift
+    Cl = L / (0.5 * rho * U_0 ** 2 * c)     # Lift coefficient
 
-    dpj = rho * U_0 * gammamatrix / dc  # Pressure difference
-    dcpj = dpj / (0.5 * rho * (U_0 ** 2))  # Pressure coefficient difference between upper and lower surface
+    dpj = rho * U_0 * gammamatrix / dc      # Pressure difference
+    dcpj = dpj/(0.5 * rho * (U_0**2))       # Pressure coefficient difference between upper and lower surface
 
     return xc4, yc4, dcpj, Cl, gammamatrix, xp, yp
-
-@nb.njit
-def compute_velocity_field(u_inf, X, Y, x_vort, y_vort, gamma_arr):
-
-    # Build velocity and pressure distribution
-    u = np.ones((len(X), len(Y))) * u_inf
-    v = np.zeros((len(X), len(Y)))
-    v_map = np.zeros((len(X), len(Y)))
-    cp_map = np.zeros((len(X), len(Y)))
-
-    print('...Creating velocity and pressure distribution.\n')
-
-    for i in range(len(X[:, 1])):
-        print('   ...Row:', i + 1)
-        for j in range(len(Y[1, :])):
-            for g, gamma in enumerate(gamma_arr):
-                uv = lumpvor2d(gamma, X[i, j], Y[i, j], x_vort[g], y_vort[g])
-                u[i, j] = u[i, j] + uv[0]
-                v[i, j] = v[i, j] + uv[1]
-
-            v_map[i, j] = np.sqrt(u[i, j] ** 2 + v[i, j] ** 2)
-            cp_map[i, j] = 1 - (v_map[i, j] / U_0) ** 2
-
-    return v_map, cp_map
 
 # ---------------------------------- #
 # Solver
@@ -226,8 +163,8 @@ def compute_velocity_field(u_inf, X, Y, x_vort, y_vort, gamma_arr):
 
 # Discretization for X using cosine distribution
 Npoints = Npan + 1
-x = np.arange(1, Npoints + 1)
-x = c / 2 * (1 - np.cos((x - 1) * np.pi / (Npoints - 1)))
+x  = np.arange(1, Npoints + 1)
+x = c/2 * (1 - np.cos((x - 1) * np.pi/(Npoints - 1)))
 
 # Flat plate
 y = np.copy(x) * 0.
@@ -237,6 +174,7 @@ if plot_cl_curve:
     print('...Building lift curve.')
     print('...Running solver.\n')
     for i, alpha in enumerate(arange):
+
         temp = calculation(y, x, Npan, Npan_flap, np.deg2rad(alpha), np.deg2rad(a_flap), c, c_flap, U_0, rho)
         cl_arr[i] = temp[3]
 
@@ -251,7 +189,27 @@ if plot_velocity_field or plot_pressure_field:
     yp = result[6]
 
     # Build velocity and pressure distribution
-    v_map, cp_map = compute_velocity_field(U_0, X, Y, xx, yy, gammaM)
+    u = np.ones((len(X), len(Y))) * U_0
+    v = np.zeros((len(X), len(Y)))
+    v_map = np.zeros((len(X), len(Y)))
+    cp_map = np.zeros((len(X), len(Y)))
+
+    print('...Creating velocity and pressure distribution.\n')
+
+    for i in range(len(X[:, 1])):
+
+        print('   ...Row:', i+1)
+
+        for j in range(len(Y[1, :])):
+
+            for g, gamma in enumerate(gammaM):
+
+                uv = indvel(gamma, X[i, j], Y[i, j], xx[g], yy[g])
+                u[i, j] = u[i, j] + uv[0]
+                v[i, j] = v[i, j] + uv[1]
+
+            v_map[i, j] = np.sqrt(u[i, j]**2 + v[i, j]**2)
+            cp_map[i, j] = 1 - (v_map[i, j]/U_0)**2
 
     print('')
 
@@ -263,6 +221,7 @@ print('...Plotting figures.')
 plt.close('all')
 
 if plot_backmesh:
+
     temp = calculation(y, x, Npan, Npan_flap, np.deg2rad(8), np.deg2rad(a_flap), c, c_flap, U_0, rho)
     xp = temp[5]
     yp = temp[6]
@@ -274,21 +233,23 @@ if plot_backmesh:
     plt.ylabel('y/c [-]')
 
 if plot_cl_curve:
+
     plt.figure("CL")
     plt.title(r'$C_l$-$\alpha$ curve')
     plt.plot(arange, cl_arr, label='Panel Method', lw=1.2)
-    plt.plot(arange, 2 * np.pi * np.deg2rad(arange), '--', label='Analytic', lw=1.2)
+    plt.plot(arange, 2*np.pi*np.deg2rad(arange), '--', label='Analytic', lw=1.2)
     plt.xlabel(r'Angle of attack $\alpha$ (deg)')
     plt.ylabel(r'Lift coefficient $C_l$ (-)')
     plt.grid('True')
     plt.legend()
 
 if plot_velocity_field:
+
     levels = 400
     men = np.mean(v_map)
     rms = np.sqrt(np.mean(v_map ** 2))
-    vmin = int(round(men - 0.5 * rms))
-    vmax = int(round(men + 0.5 * rms))
+    vmin = round(men - 0.5 * rms)
+    vmax = round(men + 0.5 * rms)
     # vmin = round(np.amin(v_map))
     # vmax = round(np.amax(v_map))
     level_boundaries = np.linspace(vmin, vmax, levels + 1)
@@ -299,15 +260,16 @@ if plot_velocity_field:
     cf = plt.contourf(X, Y, v_map, levels=levels, vmin=vmin, vmax=vmax, cmap=cmap)
     clb = plt.colorbar(
         ScalarMappable(norm=cf.norm, cmap=cf.cmap),
-        ticks=range(vmin, vmax + 2, 2),
+        ticks=range(vmin, vmax+2, 2),
         boundaries=level_boundaries,
-        values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, )
+        values=(level_boundaries[:-1] + level_boundaries[1:]) / 2,)
     clb.ax.set_title(r'$V$ (m/s)')
     plt.plot(xp, yp, '-k', lw=2.)
     plt.xlabel('x/c [-]')
     plt.ylabel('y/c [-]')
 
 if plot_pressure_field:
+
     levels = 400
     vmin = -1
     vmax = 1
@@ -321,7 +283,7 @@ if plot_pressure_field:
     cf2 = plt.contourf(X, Y, cp_map, levels=levels, vmin=vmin, vmax=vmax, cmap=cmap)
     clb = plt.colorbar(
         ScalarMappable(norm=cf2.norm, cmap=cf2.cmap),
-        ticks=range(vmin, vmax + 1),
+        ticks=range(vmin, vmax+1),
         boundaries=level_boundaries,
         values=(level_boundaries[:-1] + level_boundaries[1:]) / 2, )
     clb.ax.set_title(r'$C_p$ (-)')
@@ -330,9 +292,10 @@ if plot_pressure_field:
     plt.ylabel('y/c [-]')
 
 if plot_camber:
+
     # Calculate Y-coordinates according to naca4 airfoil
-    ycb4 = naca4(c, 0.04, 0.4, x)  # Calculate camber line for 4%
-    yflat = np.copy(x) * 0.  # Flat plate
+    ycb4 = naca4(c, 0.04, 0.4, x)   # Calculate camber line for 4%
+    yflat = np.copy(x) * 0.         # Flat plate
 
     # Results needed for plotting
     result_flat = calculation(yflat, x, Npan, Npan_flap, np.deg2rad(4), np.deg2rad(a_flap), c, c_flap, U_0, rho)
@@ -350,9 +313,10 @@ if plot_camber:
     plt.legend()
 
 if plot_deltaP:
+
     # Calculate Y-coordinates according to naca4 airfoil
-    ycb4 = naca4(c, 0.04, 0.4, x)  # Calculate camber line for 4%
-    yflat = np.copy(x) * 0.  # Flat plate
+    ycb4 = naca4(c, 0.04, 0.4, x)   # Calculate camber line for 4%
+    yflat = np.copy(x) * 0.         # Flat plate
 
     # Results needed for plotting
     result_flat = calculation(yflat, x, Npan, Npan_flap, np.deg2rad(4), np.deg2rad(a_flap), c, c_flap, U_0, rho)
@@ -362,7 +326,7 @@ if plot_deltaP:
     plt.title("Pressure Difference")
     plt.plot(result_flat[0], result_flat[2], '-b', label='Flat Plate')
     plt.plot(result_c4[0], result_c4[2], '-r', label='4% Camber')
-    plt.ylim(-3, 6)
+    plt.ylim(-3 , 6)
     plt.xlabel('x/c [-]')
     plt.ylabel(r'$Cp_l - Cp_u$ [-]')
     plt.grid('True')
